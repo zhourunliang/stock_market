2845fd615d2fa51d31b84a25eda98c4f4e5bd5c8
run <zhou_runliang@outlook.com>
2019-03-08 09:32:43 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,11 @@
+# 资产负债表
+# 如：http://quotes.money.163.com/service/zcfzb_600675.html?type=year
+download_pre_url_zcfzb = 'http://quotes.money.163.com/service/zcfzb_'
+save_folder_zcfzb = 'D:\\study\\project\\stock_market\\download\\zcfzb' 
+# 利润表
+download_pre_url_lrb = 'http://quotes.money.163.com/service/lrb_'
+save_folder_lrb = 'D:\\study\\project\\stock_market\\download\\lrb' 
+#现金流量表
+download_pre_url_xjllb = 'http://quotes.money.163.com/service/xjllb_'
+save_folder_xjllb = 'D:\\study\\project\\stock_market\\download\\xjllb' 
+
